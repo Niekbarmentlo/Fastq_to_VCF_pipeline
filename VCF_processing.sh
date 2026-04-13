ec8a98faf480e274f2eb6a6f03bc787652a9a970
Niek Barmentlo <niekbarmentlo@gmail.com>
2026-04-13 17:57:58 +0200

    Add files via upload

---
+++ VCF_processing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash --login
+#-----------------------------Mail address-----------------------------
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#-----------------------------Output files-----------------------------
+#SBATCH --output=/sbatch_scripts/Output_file/output_%j.txt
+#SBATCH --error=/sbatch_scripts/Error_output/error_output_%j.txt
+#-----------------------------Other information------------------------
+#SBATCH --comment=12
+#SBATCH --job-name=VCF_processing
+#-----------------------------Required resources-----------------------
+#SBATCH --time=4-200:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=100000
+#-----------------------------Environment, Operations and Job steps----
+
+VCFin=VCF_GWAS_40Latvia.vcf.gz
+VCFout=VCF_GWAS_40Latvia_SNPs.vcf.gz
+VCFauto=VCF_GWAS_40Latvia_SNPs_autosomal.vcf.gz
+VCFdp=VCF_GWAS_40Latvia_SNPs_autosomal.DP.vcf.gz
+
+conda activate VCF_processing #.yml not included in this GitHub repository
+
+outdir=/Latvia_GWAS
+
+cd ${outdir}
+
+rtg vcfstats ${VCFin} > rtg_results_fullVCF.out
+
+vcftools --gzvcf ${VCFin} --remove-indels --recode --recode-INFO-all --stdout | bgzip -c > ${outdir}/${VCFout}
+tabix -p vcf ${VCFout}
+
+bcftools convert -R Keep_chromosome_contigs.bed --no-version -Oz -o ${VCFauto} ${VCFout}
+tabix -p vcf ${VCFauto}
+
+vcftools --gzvcf ${VCFauto} --minDP 5 --minGQ 30 --recode --recode-INFO-all --stdout | bgzip -c > ${VCFdp}
+tabix -p vcf ${VCFdp}
